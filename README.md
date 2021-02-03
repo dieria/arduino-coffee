f4ffb40770331c1969d2be80eb3c184dca98ae49
Sebastian Barthmes <sebastian.barthmes@shk.com>
2021-02-03 16:43:55 +0100

    cleanup code

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # arduino-coffee
+
 Simple Espresso Machine extension with sensors and a display
 
 ## Timer
 
-
+Measures the time. Starts if pin 2 is low.
 
 ## Weight
 
+dout = 4
+sck = 5
+
 ## Temperature
+
+MAX6675
+Serial Output on PIN 8
+Chip Select on PIN 9
+Serial Clock on PIN 10
--- arduino-coffee.ino
+++ arduino-coffee.ino
@@ -6,52 +6,45 @@
 #include <HX711_ADC.h>
 #include <EEPROM.h>
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+// GENERAL
+String coffeebar = "1337 Coffee Bar" unsigned long printinterval = 200;
+boolean brewingInitialized = false;
+#define SERIALDEBUG false;
 
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-// The pins for I2C are defined by the Wire-library.
-// On an arduino UNO:       A4(SDA), A5(SCL)
-// On an arduino MEGA 2560: 20(SDA), 21(SCL)
-// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
+// PUMP SWITCH
+#define PUMPSWITCH_PIN 2
+boolean pumprunning = false;
+
+// TIMER
+unsigned long starttime = 0;
+unsigned long timer = 0;
+unsigned long looptime = millis();
+
+// DISPLAY
+#define SCREEN_WIDTH 128    // OLED display width, in pixels
+#define SCREEN_HEIGHT 32    // OLED display height, in pixels
 #define OLED_RESET 4        // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+// TEMPERATURE
 int max6675SO = 8;   // Serial Output am PIN 8
 int max6675CS = 9;   // Chip Select am PIN 9
 int max6675CLK = 10; // Serial Clock am PIN 10
-
-unsigned long starttime = 0;
-
-int schalterpin = 2;
-
-unsigned long timer = 0;
-
-unsigned long looptime = millis();
-
-int printinterval = 200;
-
-boolean pumprunning = false;
-
-boolean brewingInitialized = false;
-
-// Initialisierung der MAX6675 Bibliothek mit
-// den Werten der PINs
 MAX6675 ktc(max6675CLK, max6675CS, max6675SO);
 
-//init waage
+// WEIGHT SCALE
 float weight = 0;
 const int HX711_dout = 4; //mcu > HX711 dout pin
 const int HX711_sck = 5;  //mcu > HX711 sck pin
-//HX711 constructor:
 HX711_ADC LoadCell(HX711_dout, HX711_sck);
 const int calVal_eepromAdress = 0;
 long t;
 
-void tara()
+// reset and tare scale
+void tare()
 {
-  Serial.println("start tara");
+  Serial.println("start tare");
   LoadCell.begin();
   float calibrationValue; // calibration value (see example file "Calibration.ino")
   //calibrationValue = 696.0; // uncomment this if you want to set the calibration value in the sketch
@@ -70,11 +63,16 @@ void tara()
   }
 }
 
+// arduino setup
 void setup()
 {
-  Serial.begin(9600);
+  if (SERIALDEBUG == true)
+  {
+    Serial.begin(9600);
+  }
 
-  pinMode(schalterpin, INPUT_PULLUP);
+  // signal drops from 5V to 0V while pump is running
+  pinMode(PUMPSWITCH_PIN, INPUT_PULLUP);
 
   display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
 
@@ -82,19 +80,25 @@ void setup()
   display.setTextSize(1.7);            // Normal 1:1 pixel scale
   display.setTextColor(SSD1306_WHITE); // Draw white text
   display.setCursor(0, 10);            // Start at top-left corner
-  display.println("1337 Coffee Bar");
+  display.println(coffeebar);
   display.display();
 
   starttime = millis();
 
-  tara();
+  tare();
 }
 
-void printparam(double value, String paraname)
+void displayData(double temp, unsigned long timer, float weight)
 {
+  display.clearDisplay();
   display.setTextSize(1);              // Normal 1:1 pixel scale
   display.setTextColor(SSD1306_WHITE); // Draw white text
-  display.println(paraname + ": " + String(value));
+  display.setCursor(0, 0);             // Start at top-left corner
+
+  display.println("1337 Coffee Bar");
+  display.println("Temperatur: " + String(temp) + " C");
+  display.println("Timer: " + String(timer) + " s");
+  display.println("Volumen: " + String(weight) + " ml");
 }
 
 void initBrewing()
@@ -102,7 +106,7 @@ void initBrewing()
   timer = 0;
   starttime = millis();
 
-  tara();
+  tare();
 
   display.clearDisplay();
   display.setTextSize(1.7);            // Normal 1:1 pixel scale
@@ -127,15 +131,18 @@ void loop()
     // Lesen des Temperaturwertes in Grad Celsius
     double temp = ktc.readCelsius();
 
-    if (digitalRead(schalterpin) == LOW)
+    if (digitalRead(PUMPSWITCH_PIN) == LOW)
     {
       if (brewingInitialized == false)
       {
         initBrewing();
       }
-      Serial.println("TIMER");
-      Serial.println(millis());
-      Serial.println(starttime);
+      if (SERIALDEBUG == true)
+      {
+        Serial.println("TIMER");
+        Serial.println(millis());
+        Serial.println(starttime);
+      }
 
       timer = (millis() - starttime) / 1000;
     }
@@ -144,18 +151,13 @@ void loop()
       brewingInitialized = false;
     }
 
-    display.clearDisplay();
-    display.setTextSize(1);              // Normal 1:1 pixel scale
-    display.setTextColor(SSD1306_WHITE); // Draw white text
-    display.setCursor(0, 0);             // Start at top-left corner
+    displayData(temp, timer, weight);
 
-    display.println("1337 Coffee Bar");
-    display.println("Temperatur: " + String(temp) + " C");
-    display.println("Timer: " + String(timer) + " s");
-    display.println("Volumen: " + String(weight) + " ml");
-
-    Serial.print(temp);
-    Serial.println("C");
+    if (SERIALDEBUG == true)
+    {
+      Serial.print(temp);
+      Serial.println("C");
+    }
 
     display.display();
   }
@@ -172,6 +174,11 @@ void loop()
     //Serial.println(weight);
     newDataReady = 0;
   }
-Serial.println(weight);
+
+  if (SERIALDEBUG == true)
+  {
+    Serial.println(weight);
+  }
+  
   delay(20);
 }
